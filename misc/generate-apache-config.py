269fd0b65518dce66d658a3aa4612aaf144a38b1
Chris Davis <chrismd@gmail.com>
2008-07-16 13:20:47 -0500

    yet more fixes to get non-root installations working...

--- misc/generate-apache-config.py
+++ misc/generate-apache-config.py
@@ -5,7 +5,9 @@ from optparse import OptionParser
 
 option_parser = OptionParser()
 option_parser.add_option('--install-root',default='/usr/local/graphite/',
-  help="The base directory of the graphite installation, default: /usr/local/graphite/")
+  help="The base directory of the graphite installation")
+option_parser.add_option('--libs',default=None,
+  help="The directory where the graphite python package is installed (default is system site-packages)")
 
 (options,args) = option_parser.parse_args()
 
@@ -17,7 +19,18 @@ if not os.path.isdir(install_root):
   print "Graphite does not appear to be installed at %s, do you need to specify a different --install-root?" % install_root
   sys.exit(1)
 
-os.system("sed -e s!@INSTALL_ROOT@!%s!g misc/template-vhost.conf > graphite-vhost.conf" % install_root)
+python_path = [ os.path.join(install_root,'webapp') ]
+if options.libs:
+  python_path.append( options.libs )
+
+vhost = open('misc/template-vhost.conf').read()
+vhost = vhost.replace('@INSTALL_ROOT@', install_root)
+vhost = vhost.replace('@PYTHON_PATH@', str(python_path))
+
+fh = open('graphite-vhost.conf','w')
+fh.write(vhost)
+fh.close()
+
 print "Generated graphite-vhost.conf"
 print "Put this file in your apache installation's vhost include directory."
 print "NOTE: you must ensure that mod_python is properly configured before using Graphite."
