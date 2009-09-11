fd29e2cfdbf595ecfb7460da457e83c036ec4cec
Chris Davis <chrismd@gmail.com>
2009-09-11 15:22:25 -0500

    grrr. i hate distutils. going to stick with install.py for now.

--- webapp/graphite/cli/parser.py
+++ webapp/graphite/cli/parser.py
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-from pyparsing import *
+from graphite.pyparsing import *
 
 grammar = Forward()
 
