12fc9595e1e72a8ecb7531cd380904d4b59ea608
Chris Davis <chrismd@gmail.com>
2009-09-14 09:35:44 -0500

    yay i finally have distutils working decently...

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+from glob import glob
+from distutils.core import setup
+
+
+storage_dirs = [ ('storage/whisper',[]), ('storage/lists',[]), ('storage/log',[]), ('storage/rrd',[]) ]
+
+webapp_content = {}
+
+for root, dirs, files in os.walk('webapp/content'):
+  for filename in files:
+    filepath = os.path.join(root, filename)
+
+    if root not in webapp_content:
+      webapp_content[root] = []
+
+    webapp_content[root].append(filepath)
+
+
+setup(
+  name='graphite',
+  version='0.9.5',
+  url='https://launchpad.net/graphite',
+  author='Chris Davis',
+  author_email='chrismd@gmail.com',
+  license='Apache Software License 2.0',
+  description='Enterprise scalable realtime graphing',
+  package_dir={'' : 'webapp'},
+  packages=['graphite', 'graphite.account', 'graphite.browser', 'graphite.cli', 'graphite.composer', 'graphite.render', 'graphite.whitelist'],
+  package_data={'graphite' : ['templates/*']},
+  scripts=glob('bin/*'),
+  data_files=webapp_content.items() + storage_dirs,
+)
