5895eeca420b5fdc5e73137ccc99aeefa64a9b72
Chris Davis <chrismd@gmail.com>
2009-12-06 23:57:11 -0600

    this seems to fix a rarely occurring db exception bug

--- webapp/graphite/util.py
+++ webapp/graphite/util.py
@@ -21,7 +21,7 @@ from graphite.logger import log
 def getProfile(request,allowDefault=True):
   if request.user.is_authenticated():
     try:
-      return Profile.objects.get(user=request.user)
+      return request.user.profile
     except ObjectDoesNotExist:
       profile = Profile(user=request.user)
       profile.save()
