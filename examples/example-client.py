b0c7839fc3099e2bcc9ea7c5e07f586262d9eeeb
nleskiw <nleskiw@magicvoice>
2010-02-10 15:18:00 -0600

    Updated example to use uptime for load average if not using linux.

--- examples/example-client.py
+++ examples/example-client.py
@@ -15,18 +15,29 @@ limitations under the License."""
 
 import sys
 import time
+import os
+import platform 
+import subprocess
 from socket import socket
 
 CARBON_SERVER = '127.0.0.1'
 CARBON_PORT = 2003
 
-delay = 60
+delay = 60 
 if len(sys.argv) > 1:
   delay = int( sys.argv[1] )
 
 def get_loadavg():
-  # For more details, "man proc"
-  return open('/proc/loadavg').read().strip().split()[:3]
+  # For more details, "man proc" and "man uptime"  
+  if platform.system() == "Linux":
+    return open('/proc/loadavg').read().strip().split()[:3]
+  else:   
+    command = "uptime"
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    os.waitpid(process.pid, 0)
+    output = process.stdout.read().strip().split()
+    length = len(output)
+    return output[length - 3:length]
 
 sock = socket()
 try:
@@ -39,10 +50,10 @@ while True:
   now = int( time.time() )
   lines = []
   #We're gonna report all three loadavg values
-  (loadavg_1, loadavg_5, loadavg_15) = get_loadavg()
-  lines.append("system.loadavg_1min %s %d" % (loadavg_1,now))
-  lines.append("system.loadavg_5min %s %d" % (loadavg_5,now))
-  lines.append("system.loadavg_15min %s %d" % (loadavg_15,now))
+  loadavg = get_loadavg()
+  lines.append("system.loadavg_1min %s %d" % (loadavg[0],now))
+  lines.append("system.loadavg_5min %s %d" % (loadavg[1],now))
+  lines.append("system.loadavg_15min %s %d" % (loadavg[2],now))
   message = '\n'.join(lines) + '\n' #all lines must end in a newline
   print "sending message\n"
   print '-' * 80
