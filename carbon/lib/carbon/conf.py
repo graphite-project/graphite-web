91682bd96aa5caf0f03a54098c9a5ffb7baf4bb4
Chris Davis <chrismd@gmail.com>
2010-02-15 12:25:07 -0600

    config parser will now reflect proper value types based on the defaults dict

--- carbon/lib/carbon/conf.py
+++ carbon/lib/carbon/conf.py
@@ -28,6 +28,7 @@ defaults = dict(
   CACHE_QUERY_INTERFACE='0.0.0.0',
   CACHE_QUERY_PORT=7002,
   ENABLE_AMQP=False,
+  AMQP_VERBOSE=False,
   BIND_PATTERNS=['#'],
 )
 
@@ -69,16 +70,27 @@ class Settings(dict):
       raise Exception("Failed to read config file %s" % path)
 
     for key,value in parser.items(section):
-      if key.upper() == 'BIND_PATTERNS':
+      # Detect type from defaults dict
+      if key in defaults:
+        valueType = type( defaults[key] )
+      else:
+        valueType = str
+
+      if valueType is list:
         value = [ v.strip() for v in value.split(',') ]
 
-      try:
-        value = int(value)
-      except:
+      elif valueType is bool:
+        value = parser.getboolean(section, key)
+
+      else:
+        # Attempt to figure out numeric types automatically
         try:
-          value = float(value)
+          value = int(value)
         except:
-          pass
+          try:
+            value = float(value)
+          except:
+            pass
 
       self[ key.upper() ] = value
 
