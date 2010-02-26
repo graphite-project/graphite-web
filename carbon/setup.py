e48b1ecb285c88463610b9113ae4b6d77985e930
Chris Davis <chrismd@gmail.com>
2010-02-26 13:54:16 -0600

    eliminated annoying setup.py warning

--- carbon/setup.py
+++ carbon/setup.py
@@ -5,8 +5,11 @@ from glob import glob
 
 if os.environ.get('USE_SETUPTOOLS'):
   from setuptools import setup
+  setup_kwargs = dict(zip_safe=0)
+
 else:
   from distutils.core import setup
+  setup_kwargs = dict()
 
 
 storage_dirs = [ ('storage/whisper',[]), ('storage/lists',[]),
@@ -27,5 +30,5 @@ setup(
   package_data={ 'carbon' : ['*.xml'] },
   data_files=storage_dirs + conf_files,
   install_requires=['twisted', 'txamqp'],
-  zip_safe=0,
+  **setup_kwargs
 )
