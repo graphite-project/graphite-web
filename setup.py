ba58717aef7f511836430a89c09864a39b0014ca
Chris Davis <chrismd@gmail.com>
2011-01-12 02:05:46 -0600

    bumping rev of webapp to re-relase 0.9.7 as 0.9.7a with the latest fix

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ for root, dirs, files in os.walk('webapp/content'):
 
 setup(
   name='graphite-web',
-  version='0.9.7',
+  version='0.9.7a',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
