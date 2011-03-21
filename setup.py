5f2646f432adc8a1954d74791e862fd58a3ba981
Chris Davis <chrismd@gmail.com>
2011-03-20 21:57:24 -0500

    bumping webapp rev to 0.9.8pre4

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ conf_files = [ ('conf', glob('conf/*.example')) ]
 
 setup(
   name='graphite-web',
-  version='0.9.8-pre3',
+  version='0.9.8-pre4',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
