35b5f5eaccd270ef568f98d78c5b847dbf53a57f
Chris Davis <chrismd@gmail.com>
2011-04-03 21:20:50 -0500

    releasing version 0.9.8

--- whisper/setup.py
+++ whisper/setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
   name='whisper',
-  version='0.9.8-pre1',
+  version='0.9.8',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
