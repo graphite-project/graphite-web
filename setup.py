365629df1b005faaf1d79210e049174190e033c3
Chris Davis <chrismd@gmail.com>
2011-06-06 00:59:35 -0700

    bumping rev to 1.0.0-pre2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ conf_files = [ ('conf', glob('conf/*.example')) ]
 
 setup(
   name='graphite-web',
-  version='1.0.0-pre1',
+  version='1.0.0-pre2',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
