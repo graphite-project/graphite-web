310326b319d914a06af0314979fd48082078ea78
Chris Davis <chrismd@chrismd-glaptop>
2011-06-24 00:36:20 -0700

    stupid vi

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ conf_files = [ ('conf', glob('conf/*.example')) ]
 
 setup(
   name='graphite-web',
-  version='1.0.0-pre333',
+  version='1.0.0-pre3',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
