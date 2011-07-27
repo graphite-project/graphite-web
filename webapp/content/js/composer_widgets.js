42a47a19123f0a296b5a168d66158c120fcb12ec
Nicholas Leskiw <nleskiw@gmail.com>
2011-07-27 16:47:19 -0500

    Added drawNullAsZero as a checkbox to the Composer UI

--- webapp/content/js/composer_widgets.js
+++ webapp/content/js/composer_widgets.js
@@ -997,6 +997,7 @@ function createOptionsMenu() {
         menuRadioItem("line", "Slope Line (default)", "lineMode", ""),
         menuRadioItem("line", "Staircase Line", "lineMode", "staircase"),
         menuRadioItem("line", "Connected Line", "lineMode", "connected"),
+        menuCheckItem("Draw Null as Zero", "drawNullAsZero")
     ]
   });
   
