9d6b6fa383197cf235ffd7d521e1623580026ba6
Chris Davis <chrismd@gmail.com>
2011-07-30 16:27:00 -0700

    bumping whisper rev to 0.9.9-pre2

--- whisper/setup.py
+++ whisper/setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
   name='whisper',
-  version='0.9.8',
+  version='0.9.9-pre2',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
