57b19c1531e0698381286f182cab4a1a5467e2e5
Chris Davis <chrismd@gmail.com>
2011-09-15 21:12:34 -0700

    minor aesthetic cleanups

--- webapp/graphite/version/views.py
+++ webapp/graphite/version/views.py
@@ -1,12 +1,9 @@
-# Create your views here.
-
-from django.shortcuts import get_object_or_404, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
-from django.template import RequestContext
+from django.shortcuts import render_to_response
 from graphite import settings
 
+
 def index(request):
-  return render_to_response('version.html', { 'version' : settings.WEBAPP_VERSION })
-
-
+  context = {
+    'version' : settings.WEBAPP_VERSION,
+  }
+  return render_to_response('version.html', context)
