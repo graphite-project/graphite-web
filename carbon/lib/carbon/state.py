a1e14f8660fce025bb33cb0cb5b729613d1d89de
Sidnei da Silva <sidnei.da.silva@canonical.com>
2012-05-07 18:49:49 -0300

    - Moved carbon and whisper to separate projects

--- carbon/lib/carbon/state.py
+++
@@ -1,8 +0,0 @@
-__doc__ = """
-This module exists for the purpose of tracking global state used across
-several modules.
-"""
-
-metricReceiversPaused = False
-cacheTooFull = False
-connectedMetricReceiverProtocols = set()
