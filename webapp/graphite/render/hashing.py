41b7b87689afc85d66a8bc48f860dded59d3d7ff
Christopher Bowman <cbowman0@gmail.com>
2012-05-22 15:19:35 +0000

    Performance Improvement.  Keep the length of the hash ring in a static var.

--- webapp/graphite/render/hashing.py
+++ webapp/graphite/render/hashing.py
@@ -56,7 +56,9 @@ def compactHash(string):
 class ConsistentHashRing:
   def __init__(self, nodes, replica_count=100):
     self.ring = []
+    self.ring_len = len(self.ring)
     self.nodes = set()
+    self.nodes_len = len(self.nodes)
     self.replica_count = replica_count
     for node in nodes:
       self.add_node(node)
@@ -68,21 +70,25 @@ class ConsistentHashRing:
 
   def add_node(self, key):
     self.nodes.add(key)
+    self.nodes_len = len(self.nodes)
     for i in range(self.replica_count):
       replica_key = "%s:%d" % (key, i)
       position = self.compute_ring_position(replica_key)
       entry = (position, key)
       bisect.insort(self.ring, entry)
+    self.ring_len = len(self.ring)
 
   def remove_node(self, key):
     self.nodes.discard(key)
+    self.nodes_len = len(self.nodes)
     self.ring = [entry for entry in self.ring if entry[1] != key]
+    self.ring_len = len(self.ring)
 
   def get_node(self, key):
     assert self.ring
     position = self.compute_ring_position(key)
     search_entry = (position, None)
-    index = bisect.bisect_left(self.ring, search_entry) % len(self.ring)
+    index = bisect.bisect_left(self.ring, search_entry) % self.ring_len
     entry = self.ring[index]
     return entry[1]
 
@@ -90,14 +96,16 @@ class ConsistentHashRing:
     nodes = []
     position = self.compute_ring_position(key)
     search_entry = (position, None)
-    index = bisect.bisect_left(self.ring, search_entry) % len(self.ring)
-    last_index = (index - 1) % len(self.ring)
-    while len(nodes) < len(self.nodes) and index != last_index:
+    index = bisect.bisect_left(self.ring, search_entry) % self.ring_len
+    last_index = (index - 1) % self.ring_len
+    nodes_len = len(nodes)
+    while nodes_len < self.nodes_len and index != last_index:
       next_entry = self.ring[index]
       (position, next_node) = next_entry
       if next_node not in nodes:
         nodes.append(next_node)
+        nodes_len += 1
 
-      index = (index + 1) % len(self.ring)
+      index = (index + 1) % self.ring_len
 
     return nodes
