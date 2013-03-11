7f65c1508fe60758489e9a1a194fdfcedf486a7b
Sebastien FOUTRIER <s.foutrier@criteo.com>
2013-03-11 18:21:22 +0100

    Removed ugly exceptions when accessing remote metric

--- webapp/graphite/render/datalib.py
+++ webapp/graphite/render/datalib.py
@@ -18,6 +18,7 @@ import time
 from django.conf import settings
 from graphite.logger import log
 from graphite.storage import STORE, LOCAL_STORE
+from graphite.remote_storage import RemoteNode
 from graphite.render.hashing import ConsistentHashRing
 from graphite.util import unpickle
 
@@ -227,12 +228,15 @@ def fetchData(requestContext, pathExpr):
   for dbFile in store.find(pathExpr):
     log.metric_access(dbFile.metric_path)
     dbResults = dbFile.fetch( timestamp(startTime), timestamp(endTime) )
-    try:
-      cachedResults = CarbonLink.query(dbFile.real_metric)
-      results = mergeResults(dbResults, cachedResults)
-    except:
-      log.exception()
+    if isinstance(dbFile, RemoteNode):
       results = dbResults
+    else:
+      try:
+        cachedResults = CarbonLink.query(dbFile.real_metric)
+        results = mergeResults(dbResults, cachedResults)
+      except:
+        log.exception()
+        results = dbResults
 
     if not results:
       continue
