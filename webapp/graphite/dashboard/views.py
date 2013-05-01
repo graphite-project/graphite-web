3ee8f2e5c6590d54fdca7a7ae81980c960dfb02e
Travis Stockwell <travis.stockwell@optum.com>
2013-05-01 12:44:17 -0600

    Per Jeff Schoeder's suggestion - just use a fast replace, rather than a regex.

--- webapp/graphite/dashboard/views.py
+++ webapp/graphite/dashboard/views.py
@@ -144,7 +144,7 @@ def dashboard(request, name=None):
     try:
       dashboard = Dashboard.objects.get(name=name)
     except Dashboard.DoesNotExist:
-      name = re.sub(r'"', '', name)
+      name = name.replace('"', '')
       context['initialError'] = "Dashboard '%s' does not exist." % name
     else:
       context['initialState'] = dashboard.state
