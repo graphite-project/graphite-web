8346ca98f25f0059994aab89358bfb1048285074
Jeff Schroeder <jeffschroeder@computer.org>
2013-05-09 22:46:23 -0500

    Cleaning up build-index.sh a bit

--- bin/build-index.sh
+++ bin/build-index.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-if [ "$GRAPHITE_ROOT" = "" ]
-then
-  GRAPHITE_ROOT="/opt/graphite"
-fi
-
-if [ "$GRAPHITE_STORAGE_DIR" = "" ]
-then
-  GRAPHITE_STORAGE_DIR="${GRAPHITE_ROOT}/storage"
-fi
-
-
+GRAPHITE_ROOT="${GRAPHITE_ROOT:-/opt/graphite}"
+# We're using bash so we might as well use default variable features
+GRAPHITE_STORAGE_DIR="${GRAPHITE_STORAGE_DIR:-${GRAPHITE_ROOT}/storage}"
 WHISPER_DIR="${GRAPHITE_STORAGE_DIR}/whisper"
 CERES_DIR="${GRAPHITE_STORAGE_DIR}/ceres"
-
-if [ ! -d "$WHISPER_DIR" ] || [ ! -d "$CERES_DIR" ]
-then
-  echo "Fatal Error: neither $WHISPER_DIR nor $CERES_DIR exist."
-  exit 1
-fi
-
 INDEX_FILE="${GRAPHITE_STORAGE_DIR}/index"
 TMP_INDEX="${GRAPHITE_STORAGE_DIR}/.index.tmp"
 
+if [ ! -d "$WHISPER_DIR" ] || [ ! -d "$CERES_DIR" ]; then
+    echo "Fatal Error: neither $WHISPER_DIR nor $CERES_DIR exist." >&2
+    exit 1
+elif [ -z "$(which perl 2>/dev/null)" ]; then
+    echo "Fatal Error: perl is required to run this." >&2
+    exit 1
+fi
+
 rm -f $TMP_INDEX
 touch $INDEX_FILE
-echo "[`date`]  building index..."
-cd $WHISPER_DIR
-find -L . -name '*.wsp' | perl -pe 's!^[^/]+/(.+)\.wsp$!$1!; s!/!.!g' > $TMP_INDEX
-cd $CERES_DIR
-find -L . -name '.ceres-node' | perl -pe 's!^[^/]+/(.+)/\.ceres-node$!$1!; s!/!.!g;' >> $TMP_INDEX
-echo "[`date`]  complete, switching to new index file"
+echo "[$(date)]  building index..."
+
+if [ -d "$WHISPER_DIR" ]; then
+    cd $WHISPER_DIR
+    find -L . -name '*.wsp' | perl -pe 's!^[^/]+/(.+)\.wsp$!$1!; s!/!.!g' > $TMP_INDEX
+fi
+if [ -d "$CERES_DIR" ]; then
+    cd $CERES_DIR
+    find -L . -name '.ceres-node' | perl -pe 's!^[^/]+/(.+)/\.ceres-node$!$1!; s!/!.!g;' >> $TMP_INDEX
+fi
+
+echo "[$(date)]  complete, switching to new index file"
 mv -f $TMP_INDEX $INDEX_FILE
