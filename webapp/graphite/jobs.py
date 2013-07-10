459aa325b4b9550cdf4b0990b982fbe4c367eccc
Tom Naessens <Tom.Naessens@UGent.be>
2013-07-10 11:40:21 +0200

    Implemented the limitation of jobs results in the filetree

--- webapp/graphite/jobs.py
+++ webapp/graphite/jobs.py
@@ -12,12 +12,12 @@ user_dict = {
 jobs_dict = {
   "Venosaur": {
     'nodes': ["serenity", "carbon", "think"],
-    'startTime': 0,
+    'startTime': 2,
     'endTime': 1373444994
   },
   "Charizard": {
     'nodes': ["carbon"],
-    'startTime': 0,
+    'startTime': 1,
     'endTime': 1373444994
   },
   "Blastoise":
@@ -34,11 +34,24 @@ def get_jobs(user, limit=False):
   If the limit paramter is set, display the most recent limit number of jobs
   """
   try:
+    jobs = []
+
     # Return all jobs for the admins
     if user.has_perm('account.can_see_all'):
-      return jobs_dict.keys()
+      jobs = jobs_dict.keys()
     else:
-      return user_dict[user.username]
+      jobs = user_dict[user.username]
+
+    # Sort them by startTime
+    jobs = sorted(jobs, key=lambda k: jobs_dict[k]['startTime'])
+    jobs.reverse()
+
+    # Did we limit the jobs? Return only the last limit number of jobs!
+    if limit:
+      return jobs[:limit]
+    
+    return jobs
+
   except KeyError:
     # We log to info here; as it is possible that a user just doesn't has any jobs
     log.info("No jobs found for user %s", user)
