fc3f018544c19b90cc63797d18970a4cc27ef2ad
Victor Bazterra <victor.bazterra@mathworks.com>
2013-08-19 09:56:20 -0400

    Changes needed to run without warning using django 1.5.

--- webapp/graphite/browser/urls.py
+++ webapp/graphite/browser/urls.py
@@ -12,7 +12,7 @@ You may obtain a copy of the License at
    See the License for the specific language governing permissions and
    limitations under the License."""
 
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('graphite.browser.views',
   ('^header/?$', 'header'),
--- webapp/graphite/manage.py
+++ webapp/graphite/manage.py
@@ -7,5 +7,7 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
+import os
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    execute_from_command_line()
