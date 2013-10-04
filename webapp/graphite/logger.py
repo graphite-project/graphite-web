799ffdedd5676eae2afac5f0959f32b6271623fb
James Juran <james.juran@mandiant.com>
2013-10-04 08:47:41 -0400

    Move log config to settings so it can be changed
    Without users having to modify code. For example, I want to log to a
    SysLogHandler to avoid issue #448, but I only want to change my
    local_settings.py file to do it.

--- webapp/graphite/logger.py
+++ webapp/graphite/logger.py
@@ -22,39 +22,12 @@ logging.addLevelName(30,"metric_access")
 
 class GraphiteLogger:
   def __init__(self):
-    #Setup log files
-    self.infoLogFile = os.path.join(settings.LOG_DIR,"info.log")
-    self.exceptionLogFile = os.path.join(settings.LOG_DIR,"exception.log")
-    self.cacheLogFile = os.path.join(settings.LOG_DIR,"cache.log")
-    self.renderingLogFile = os.path.join(settings.LOG_DIR,"rendering.log")
-    self.metricAccessLogFile = os.path.join(settings.LOG_DIR,"metricaccess.log")
     #Setup loggers
     self.infoLogger = logging.getLogger("info")
-    self.infoLogger.setLevel(logging.INFO)
     self.exceptionLogger = logging.getLogger("exception")
     self.cacheLogger = logging.getLogger("cache")
     self.renderingLogger = logging.getLogger("rendering")
     self.metricAccessLogger = logging.getLogger("metric_access")
-    #Setup formatter & handlers
-    self.formatter = logging.Formatter("%(asctime)s :: %(message)s","%a %b %d %H:%M:%S %Y")
-    self.infoHandler = Rotater(self.infoLogFile,when="midnight",backupCount=1)
-    self.infoHandler.setFormatter(self.formatter)
-    self.infoLogger.addHandler(self.infoHandler)
-    self.exceptionHandler = Rotater(self.exceptionLogFile,when="midnight",backupCount=1)
-    self.exceptionHandler.setFormatter(self.formatter)
-    self.exceptionLogger.addHandler(self.exceptionHandler)
-    if settings.LOG_CACHE_PERFORMANCE:
-      self.cacheHandler = Rotater(self.cacheLogFile,when="midnight",backupCount=1)
-      self.cacheHandler.setFormatter(self.formatter)
-      self.cacheLogger.addHandler(self.cacheHandler)
-    if settings.LOG_RENDERING_PERFORMANCE:
-      self.renderingHandler = Rotater(self.renderingLogFile,when="midnight",backupCount=1)
-      self.renderingHandler.setFormatter(self.formatter)
-      self.renderingLogger.addHandler(self.renderingHandler)
-    if settings.LOG_METRIC_ACCESS:
-      self.metricAccessHandler = Rotater(self.metricAccessLogFile,when="midnight",backupCount=10)
-      self.metricAccessHandler.setFormatter(self.formatter)
-      self.metricAccessLogger.addHandler(self.metricAccessHandler)
 
   def info(self,msg,*args,**kwargs):
     return self.infoLogger.info(msg,*args,**kwargs)
