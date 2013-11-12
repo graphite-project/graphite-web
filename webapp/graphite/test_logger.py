ed748238ad26077d8fcd6c48600f9c8ac1a9c6d4
Valentin Haenel <valentin.haenel@gmx.de>
2013-11-12 17:23:56 +0100

    improve tests
    Test various aspects of the logger module.

--- webapp/graphite/test_logger.py
+++ webapp/graphite/test_logger.py
@@ -1,14 +1,17 @@
 import unittest
 import tempfile
+import os.path
+import glob
+import re
 
 from django.conf import settings
 # This line has to occur before importing functions and datalib.
 temp_dir = tempfile.mkdtemp(prefix='graphite-log-test')
 settings.configure(
     LOG_DIR=temp_dir,
-    LOG_CACHE_PERFORMANCE='',
-    LOG_RENDERING_PERFORMANCE='',
-    LOG_METRIC_ACCESS='',
+    LOG_CACHE_PERFORMANCE=False,
+    LOG_RENDERING_PERFORMANCE=False,
+    LOG_METRIC_ACCESS=False,
     LOG_ROTATE=True,
     )
 
@@ -25,3 +28,22 @@ class TestLogger(unittest.TestCase):
     def test_config_logger(self):
         pass
 
+    def test_info_log(self):
+        message = 'Test Info Message'
+        log.info(message)
+        lines = [l for l in open(os.path.join(temp_dir, 'info.log'))]
+        self.assertEqual(message, lines[0].split('::')[1].strip())
+
+    def test_metric_log(self):
+        message = 'Test Info Message'
+        log.metric_access(message)
+        file_name = os.path.join(temp_dir, 'metricaccess.log')
+        self.assertFalse(os.path.exists(file_name))
+
+    def test_rotate(self):
+        handler = log.infoLogger.handlers[0]
+        handler.doRollover()
+        files = glob.glob(os.path.join(temp_dir, 'info.log.*'))
+        matches = [re.match('info.log.[0-9]{4}-[0-9]{2}-[0-9]{2}',
+                   os.path.basename(f)) for f in files]
+        self.assertTrue(any(matches))
