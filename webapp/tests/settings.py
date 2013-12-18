f4ba6a295424b568715fcb2ce792b743a8795975
Bruno Renié <brutasse@gmail.com>
2013-12-18 17:56:22 +0100

    Add a basic test for the render view with whisper data

--- webapp/tests/settings.py
+++ webapp/tests/settings.py
@@ -1,6 +1,12 @@
-from graphite.settings import *
+from graphite.settings import *  # noqa
 
 from django import VERSION
 
 if VERSION < (1, 6):
     TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    },
+}
