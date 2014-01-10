ba3acd257530448e82e5654e4d74a57df9803106
Valentin Haenel <valentin.haenel@gmx.de>
2014-01-10 15:16:59 +0100

    try fixing the import error for Django

--- webapp/graphite/cli/urls.py
+++ webapp/graphite/cli/urls.py
@@ -12,7 +12,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import *
+except ImportError:
+    from django.conf.urls import *
 
 urlpatterns = patterns('graphite.cli.views',
      (r'^autocomplete/?$', 'autocomplete'),
