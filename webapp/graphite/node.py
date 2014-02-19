67fc6692ab5ba123a0b3f86fbc88a866bad2b251
Kevin J. Rice <justanyone@users.noreply.github.com>
2014-02-19 17:24:09 -0600

    Added code to webapp/graphite/node.py to make it avoid getting intervals until the reader needs them.  Required by MongoDB reader, wherein getting the intervals would be just as expensive as getting the data in the first place.

--- webapp/graphite/node.py
+++ webapp/graphite/node.py
@@ -1,13 +1,19 @@
 
+from graphite.logger import log
+from graphite.intervals import Interval, IntervalSet
+import time
+import traceback
+
 
 class Node(object):
-  __slots__ = ('name', 'path', 'local', 'is_leaf')
+  __slots__ = ('name', 'path', 'local', 'is_leaf', 'avoidIntervals')
 
   def __init__(self, path):
     self.path = path
     self.name = path.split('.')[-1]
     self.local = True
     self.is_leaf = False
+    self.avoidIntervals = False
 
   def __repr__(self):
     return '<%s[%x]: %s>' % (self.__class__.__name__, id(self), self.path)
@@ -20,14 +26,33 @@ class BranchNode(Node):
 class LeafNode(Node):
   __slots__ = ('reader', 'intervals')
 
-  def __init__(self, path, reader):
-    Node.__init__(self, path)
-    self.reader = reader
-    self.intervals = reader.get_intervals()
-    self.is_leaf = True
+  def __init__(self, path, reader, avoidIntervals=False):
+    try:
+      Node.__init__(self, path)
+      self.reader = reader
+      self.avoidIntervals = avoidIntervals
+      if self.avoidIntervals:
+        now = int(time.time())
+        intervals = [Interval(0, now)]
+        self.intervals = IntervalSet(intervals)               
+      else:
+        self.intervals = reader.get_intervals()
+      self.is_leaf = True
+    except:
+      log.info("EXCEPTION: LeafNode.__init__() exception: %s" % (traceback.format_exc()))
+      return
 
   def fetch(self, startTime, endTime):
-    return self.reader.fetch(startTime, endTime)
+    retval = []
+    try:
+      if self.avoidIntervals:
+        self.intervals = self.reader.get_intervals(startTime=startTime, endTime=endTime)
+      log.info("LeafNode.fetch(): have intervals path %s, st=%s, et=%s, intervals: %s" % (self.path, startTime, endTime, self.intervals))
+      retval = self.reader.fetch(startTime, endTime)
+      self.reader.fetch(startTime, endTime)
+    except:
+      log.info("EXCEPTION:  LeafNode.fetch() exception: %s" % (traceback.format_exc()))
+    return retval
 
   def __repr__(self):
     return '<LeafNode[%x]: %s (%s)>' % (id(self), self.path, self.reader)
