939f5a77d14a5a9b8763428ec9fd60a8df05fe7a
fiaz.hossain <fiaz.hossain@salesforce.com>
2014-02-21 12:23:08 -0800

    Fix multiplySeriesWithWildcards help message

--- webapp/graphite/render/functions.py
+++ webapp/graphite/render/functions.py
@@ -246,7 +246,7 @@ def multiplySeriesWithWildcards(requestContext, seriesList, *position): #XXX
 
   .. code-block:: none
 
-    &target=multiplySeriesWithWildcards([web.host-[0-7].avg-response.value, web.host-[0-7].total-request.value], 1)
+    &target=multiplySeriesWithWildcards([web.host-[0-7].avg-response.value, web.host-[0-7].total-request.value], 2,3)
 
   This would be the equivalent of
   ``target=multiplySeries(web.host-0.avg-response.value, web.host-0.total-request.value)&target=multiplySeries(web.host-1.avg-response.value, web.host-1.total-request.value)...``
