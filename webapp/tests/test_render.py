31b006f99a2dc84d2937cb481d21ea4d82f87dcf
Kevin J. Rice <justanyone@users.noreply.github.com>
2014-02-28 08:19:12 -0600

    mods to test_render, which would fail if the TravisCI was running so slowly that there was more than a 1 second delay between saving to whisper and retriving from it.  thus, the intermittent failure.  Fixed now with more robust solution that also verifies timestamps returned are sequentially increasing.

--- webapp/tests/test_render.py
+++ webapp/tests/test_render.py
@@ -26,6 +26,10 @@ class RenderTest(TestCase):
         except OSError:
             pass
 
+    def assertEqual(self, actual, shouldBe, msg=None):
+        out = "Actual: %s, should be: %s; msg: %s" % (actual, shouldBe, msg)
+        assert actual == shouldBe, out
+
     def test_render_view(self):
         url = reverse('graphite.render.views.renderView')
 
@@ -38,13 +42,30 @@ class RenderTest(TestCase):
         self.addCleanup(self.wipe_whisper)
         whisper.create(self.db, [(1, 60)])
 
-        ts = int(time.time())
-        whisper.update(self.db, 0.5, ts - 2)
-        whisper.update(self.db, 0.4, ts - 1)
-        whisper.update(self.db, 0.6, ts)
+        saveTs = int(time.time())
+        whisper.update(self.db, 0.5, saveTs - 2)
+        whisper.update(self.db, 0.4, saveTs - 1)
+        whisper.update(self.db, 0.6, saveTs - 0)
+
+        # Note: If saving to whisper takes over 1 second, data is not 
+        # at the end of the returned-data array.  Therefore, assemble data
+        # for verification.
 
         response = self.client.get(url, {'target': 'test', 'format': 'json'})
         data = json.loads(response.content)
-        end = data[0]['datapoints'][-4:]
-        self.assertEqual(
-            end, [[None, ts - 3], [0.5, ts - 2], [0.4, ts - 1], [0.6, ts]])
+        dd = {}
+        lastTs = 0
+        populated = [ saveTs , saveTs-1, saveTs-2 ]
+        for val, ts in data[0]['datapoints']:
+            #print "val: %s, ts: %s" % (val, ts)
+            dd[ts] = val
+            assert ts > lastTs, "timestamps should increase."
+            lastTs = ts
+            if ts not in populated:
+                assert val is None
+        msg = "Orig saveTs: %s.  Data at time %s, offset %d"
+        self.assertEqual(dd[saveTs - 2], 0.5, msg % (saveTs, saveTs-2, 2)) 
+        self.assertEqual(dd[saveTs - 1], 0.4, msg % (saveTs, saveTs-1, 1))
+        self.assertEqual(dd[saveTs - 0], 0.6, msg % (saveTs, saveTs-0, 0))
+        return True
+
