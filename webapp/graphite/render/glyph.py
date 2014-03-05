eca08672a58e0209699abddc03f26839123c383f
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2014-03-05 19:02:52 +0100

    Ignore logBase less than one

--- webapp/graphite/render/glyph.py
+++ webapp/graphite/render/glyph.py
@@ -157,7 +157,7 @@ class Graph:
     if self.logBase:
       if self.logBase == 'e':
         self.logBase = math.e
-      elif self.logBase <= 0:
+      elif self.logBase < 1:
         self.logBase = None
         params['logBase'] = None
       else:
