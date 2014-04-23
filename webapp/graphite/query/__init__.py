8942deda2ce775e4ce6c4e1a10e91f6e3332508d
Edin Salkovic <edin.salkovic@gmail.com>
2014-04-23 09:10:18 +0200

    Added some documentation.

--- webapp/graphite/query/__init__.py
+++ webapp/graphite/query/__init__.py
@@ -1,9 +1,20 @@
+""" This package (query) is called render in graphite-web """
 import pytz
 from graphite import settings
 from graphite.query.evaluator import evaluateTarget
 from graphite.query.attime import parseATTime
 
 def query(params):
+    """ Returns a list of graphite.query.datalib.TimeSeries instances
+
+        params is a dictionary whose both keys and values are strings,
+        except which were taken
+        from http://graphite.readthedocs.org/en/latest/render_api.html
+        (except for the graph/format parameters):
+        maxDataPoints: see http://graphite.readthedocs.org/en/latest/render_api.html#maxdatapoints
+        tz: see http://graphite.readthedocs.org/en/latest/render_api.html#tz
+        until/from: see http://graphite.readthedocs.org/en/latest/render_api.html#from-until
+    """
     if 'maxDataPoints' in params and params['maxDataPoints'].isdigit():
         params['maxDataPoints'] = int(params['maxDataPoints'])
 
@@ -16,7 +27,7 @@ def query(params):
         except pytz.UnknownTimeZoneError:
           pass
     params['tzinfo'] = tzinfo
-    
+
     if 'until' in params:
         untilTime = parseATTime(params['until'], tzinfo)
     else:
