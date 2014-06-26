576ec1a5cd4d38ec20230794d3b7017f105ffa22
Dave Ertel <dertel@kixeye.com>
2014-06-26 17:16:31 +1000

    Backport of c5d9e7e PR719 to 0.9.x

--- webapp/graphite/render/hashing.py
+++ webapp/graphite/render/hashing.py
@@ -19,12 +19,13 @@ try:
   from hashlib import md5
 except ImportError:
   from md5 import md5
+from itertools import chain
 import bisect
 
 def hashRequest(request):
   # Normalize the request parameters so ensure we're deterministic
   queryParams = ["%s=%s" % (key, '&'.join(values))
-                 for (key,values) in request.GET.lists()
+                 for (key,values) in chain(request.POST.lists(), request.GET.lists())
                  if not key.startswith('_')]
 
   normalizedParams = ','.join( sorted(queryParams) ) or 'noParam'
