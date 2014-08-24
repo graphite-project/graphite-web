30514a664a2fbb23bf4b812adc9dc3d48e4e6e22
Jilles Oldenbeuving <ojilles@gmail.com>
2014-08-24 22:12:57 +0200

    Backport to 0.9.x: Implements missing minimumBelow() render function
    As discussed (for over a year) on issue #255, minimumBelow() is missing
    which is odd as it's counterparts are all there. This change
    implements that function and adds it to the composer.

--- webapp/content/js/composer_widgets.js
+++ webapp/content/js/composer_widgets.js
@@ -1091,6 +1091,7 @@ function createFunctionsMenu() {
         {text: 'Maximum Value Above', handler: applyFuncToEachWithInput('maximumAbove', 'Draw all metrics whose maximum value is above ___')},
         {text: 'Maximum Value Below', handler: applyFuncToEachWithInput('maximumBelow', 'Draw all metrics whose maximum value is below ___')},
         {text: 'Minimum Value Above', handler: applyFuncToEachWithInput('minimumAbove', 'Draw all metrics whose minimum value is above ___')},
+        {text: 'Minimum Value Below', handler: applyFuncToEachWithInput('minimumBelow', 'Draw all metrics whose minimum value is below ___')},
         {text: 'sortByMaxima', handler: applyFuncToEach('sortByMaxima')},
         {text: 'sortByMinima', handler: applyFuncToEach('sortByMinima')},
         {text: 'limit', handler: applyFuncToEachWithInput('limit', 'Limit to first ___ of a list of metrics')},
