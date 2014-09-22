a9fd8d332540615bb14d6051cfe77374e46c2b8f
Bryan Irvine <sparctacus@gmail.com>
2014-09-22 14:27:33 -0700

    add support for posting when
    fixed support for back-filling events.

--- webapp/graphite/events/views.py
+++ webapp/graphite/events/views.py
@@ -48,8 +48,7 @@ def post_event(request):
         values["what"] = event["what"]
         values["tags"] = event.get("tags", None)
         values["when"] = datetime.datetime.fromtimestamp(
-            event.get("when", time.time())) if not event.get("when") 
-            else datetime.datetime.fromtimestamp(float(event["when"]))
+            event.get("when", time.time())) if not event.get("when") else datetime.datetime.fromtimestamp(float(event["when"]))
         if "data" in event:
             values["data"] = event["data"]
 
