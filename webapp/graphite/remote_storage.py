graphite-project/graphite-web

commit a835a884953e148134c619c852f70c8911271b00

remote_storage.py - optionally use POST instead of GET for find and render requests

webapp/graphite/remote_storage.py
@@ -57,7 +57,10 @@ class FindRequest:
     query_string = urlencode(query_params)
 
     try:
-      self.connection.request('GET', '/metrics/find/?' + query_string)
+      if settings.REMOTE_STORE_USE_POST:
+        self.connection.request('POST', '/metrics/find/', query_string)
+      else:
+        self.connection.request('GET', '/metrics/find/?' + query_string)
     except:
       self.store.fail()
       if not self.suppressErrors:
@@ -120,7 +123,10 @@ class RemoteNode:
 
     connection = HTTPConnectionWithTimeout(self.store.host)
     connection.timeout = settings.REMOTE_STORE_FETCH_TIMEOUT
-    connection.request('GET', '/render/?' + query_string)
+    if settings.REMOTE_STORE_USE_POST:
+      self.connection.request('POST', '/render/', query_string)
+    else:
+      self.connection.request('GET', '/render/?' + query_string)
     response = connection.getresponse()
     assert response.status == 200, "Failed to retrieve remote data: %d %s" % (response.status, response.reason)
     rawData = response.read()

webapp/graphite/local_settings.py.example
@@ -175,6 +175,7 @@
 #REMOTE_STORE_FETCH_TIMEOUT = 6   # Timeout to fetch series data
 #REMOTE_STORE_FIND_TIMEOUT = 2.5  # Timeout for metric find requests
 #REMOTE_STORE_RETRY_DELAY = 60    # Time before retrying a failed remote webapp
+#REMOTE_STORE_USE_POST = False    # Use POST instead of GET for remote requests
 #REMOTE_FIND_CACHE_DURATION = 300 # Time to cache remote metric find results
 
 ## Remote rendering settings

webapp/graphite/settings.py
@@ -65,6 +65,7 @@ LOG_CACHE_PERFORMANCE = False
 REMOTE_STORE_FETCH_TIMEOUT = 6
 REMOTE_STORE_FIND_TIMEOUT = 2.5
 REMOTE_STORE_RETRY_DELAY = 60
+REMOTE_STORE_USE_POST = False
 REMOTE_FIND_CACHE_DURATION = 300
 
 #Remote rendering settings

commit 93ea43eb880be7eee4e6f0e9841eee701a7a98f4

Fix bug introduced in a835a88

webapp/graphite/remote_storage.py
@@ -136,9 +136,9 @@ class RemoteNode:
     connection = HTTPConnectionWithTimeout(self.store.host)
     connection.timeout = settings.REMOTE_STORE_FETCH_TIMEOUT
     if settings.REMOTE_STORE_USE_POST:
-      self.connection.request('POST', '/render/', query_string)
+      connection.request('POST', '/render/', query_string)
     else:
-      self.connection.request('GET', '/render/?' + query_string)
+      connection.request('GET', '/render/?' + query_string)
     response = connection.getresponse()
     assert response.status == 200, "Failed to retrieve remote data: %d %s" % (response.status, response.reason)
     rawData = response.read()