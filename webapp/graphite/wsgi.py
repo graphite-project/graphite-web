8187904ee03655390ab2561cea21d573a371d12b
John Gallagher <john@foursquare.com>
2015-03-23 18:37:49 -0400

    Remove the metrics/search endpoint
    it isn't use by graphite-web itself,
    and the cost of initializing it when a tree
    contains millions of metrics, even when explicitly
    intialized in wsgi, seems to be prohibitive.
    
    Its functionality seems to be superceded by browser/search
    (grep over the index file)
    and metrics find
    (uses the storage engine for searching)

--- webapp/graphite/wsgi.py
+++ webapp/graphite/wsgi.py
@@ -9,7 +9,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graphite.settings')  # noqa
 
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
-from graphite.logger import log
 
 application = get_wsgi_application()
 
@@ -27,9 +26,3 @@ else:
         directory = os.path.join(os.path.dirname(module.__file__), 'static')
         if os.path.isdir(directory):
             application.add_files(directory, prefix=prefix)
-
-# Initializing the search index can be very expensive. The import below
-# ensures the index is preloaded before any requests are handed to the
-# process.
-log.info("graphite.wsgi - pid %d - reloading search index" % os.getpid())
-import graphite.metrics.search  # noqa
