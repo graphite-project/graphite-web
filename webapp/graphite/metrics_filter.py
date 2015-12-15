95485fb3b14451ee3ba9c9d73ec9a619fe082a4f
alberto <apascualco@gmail.com>
2015-12-15 14:29:15 +0100

    Fix indentation contains tabs

--- webapp/graphite/metrics_filter.py
+++ webapp/graphite/metrics_filter.py
@@ -6,32 +6,32 @@ from graphite.metric_filter.models import GroupMetricFilter
 
 
 def nodeFilter(user, nodePath):
-    if settings.ALLOW_METRICFILTER_BY_USER:
-        if user.id is not None:
-            filters = UserMetricFilter.getFilters(user.id)
-            if filters is not None:
-                for filter in filters.all():
-                    matching = match(nodePath, filter)
-                    if matching is not None:
-                        return matching
-        for filters in GroupMetricFilter.getFiltersByUser(user):
-            if filters is not None:
-                for filter in filters.all():
-                    if filter is not None:
-                        matching = match(nodePath, filter)
-                        if matching is not None:
-                            return matching
-        return False
-    else:
-        return True
+  if settings.ALLOW_METRICFILTER_BY_USER:
+    if user.id is not None:
+      filters = UserMetricFilter.getFilters(user.id)
+      if filters is not None:
+        for filter in filters.all():
+          matching = match(nodePath, filter)
+          if matching is not None:
+            return matching
+    for filters in GroupMetricFilter.getFiltersByUser(user):
+      if filters is not None:
+        for filter in filters.all():
+          if filter is not None:
+            matching = match(nodePath, filter)
+            if matching is not None:
+              return matching
+    return False
+  else:
+    return True
 
 def match(path, filter):
-    #log.info('testing %s' % filter.filter)
-    pattern = re.compile(filter.filter)
-    if pattern.match(path):
-        if filter.type_filter == 0:
-            return True
-        elif filter.type_filter == 1:
-            return False
-    else:
-        return None
+  #log.info('testing %s' % filter.filter)
+  pattern = re.compile(filter.filter)
+  if pattern.match(path):
+    if filter.type_filter == 0:
+      return True
+    elif filter.type_filter == 1:
+      return False
+  else:
+    return None
