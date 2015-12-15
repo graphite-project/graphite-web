60c9967c915a5dc6750f1992c3d18b244d811e83
alberto <apascualco@gmail.com>
2015-12-15 14:34:42 +0100

    Fix indentation contains tabs

--- webapp/graphite/metric_filter/admin.py
+++ webapp/graphite/metric_filter/admin.py
@@ -5,17 +5,17 @@ from graphite.metric_filter.models import GroupMetricFilter
 
 
 class MetricFilterAdmin(admin.ModelAdmin):
-    list_display = ('filter', 'description', 'type_filter')
-    search_fields = ('filter', 'description', 'type_filter')
-    ordering = ('id',)
+  list_display = ('filter', 'description', 'type_filter')
+  search_fields = ('filter', 'description', 'type_filter')
+  ordering = ('id',)
 
 
 class UserMetricFilterAdmin(admin.ModelAdmin):
-    filter_horizontal = ('metric_filter',)
+  filter_horizontal = ('metric_filter',)
 
 
 class GroupMetricFilterAdmin(admin.ModelAdmin):
-    filter_horizontal = ('metric_filter',)
+  filter_horizontal = ('metric_filter',)
 
 admin.site.register(MetricFilter, MetricFilterAdmin)
 admin.site.register(UserMetricFilter, UserMetricFilterAdmin)
