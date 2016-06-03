17de77d8bb6f49ddd954f0fb7b8a462a9bc0d554
Jorn Wijnands <jwijnands@ebay.com>
2016-06-03 16:40:43 +0200

    Fix brance expansion
      * Expand brances based on https://bugs.python.org/issue9584
    
      Allow multiple braces (both serial and nested) to be expanded

--- webapp/graphite/storage.py
+++ webapp/graphite/storage.py
@@ -1,4 +1,4 @@
-import os, time, fnmatch, socket, errno
+import os, time, fnmatch, socket, errno, re
 from django.conf import settings
 from os.path import isdir, isfile, join, exists, splitext, basename, realpath
 import whisper
@@ -253,23 +253,40 @@ def _deduplicate(entries):
 
 
 def match_entries(entries, pattern):
-  # First we check for pattern variants (ie. {foo,bar}baz = foobaz or barbaz)
-  v1, v2 = pattern.find('{'), pattern.find('}')
-
-  if v1 > -1 and v2 > v1:
-    variations = pattern[v1+1:v2].split(',')
-    variants = [ pattern[:v1] + v + pattern[v2+1:] for v in variations ]
+    # First we check for pattern variants (ie. {foo,bar}baz = foobaz or barbaz)
     matching = []
 
-    for variant in variants:
-      matching.extend( fnmatch.filter(entries, variant) )
+    for variant in expand_braces(pattern):
+        matching.extend(fnmatch.filter(entries, variant))
 
-    return list( _deduplicate(matching) ) #remove dupes without changing order
+    return list(_deduplicate(matching))
 
-  else:
-    matching = fnmatch.filter(entries, pattern)
-    matching.sort()
-    return matching
+
+"""
+    Brace expanding patch for python3 borrowed from:
+    https://bugs.python.org/issue9584
+"""
+def expand_braces(orig):
+    r = r'.*(\{.+?[^\\]\})'
+    p = re.compile(r)
+
+    s = orig[:]
+    res = list()
+
+    m = p.search(s)
+    if m is not None:
+        sub = m.group(1)
+        open_brace = s.find(sub)
+        close_brace = open_brace + len(sub) - 1
+        if sub.find(',') != -1:
+            for pat in sub.strip('{}').split(','):
+                res.extend(expand_braces(s[:open_brace] + pat + s[close_brace + 1:]))
+        else:
+            res.extend(expand_braces(s[:open_brace] + sub.replace('}', '\\}') + s[close_brace + 1:]))
+    else:
+        res.append(s.replace('\\}', '}'))
+
+    return list(set(res))
 
 
 # Node classes
