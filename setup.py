02f5b99a0dacb6343ba833fa106db749da90a8dc
nickjacobson <nicksjacobson@yahoo.com>
2016-06-17 14:09:57 -0700

    Adding more trove classifiers.
    Adding trove classifers from https://github.com/graphite-project/carbon/commit/e9904725e371974036ea8312231b092715a4b88c

--- setup.py
+++ setup.py
@@ -93,8 +93,13 @@ try:
       scripts=glob('bin/*'),
       data_files=webapp_content.items() + storage_dirs + conf_files + examples,
       classifiers=[
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 2 :: Only',
           ],
       **setup_kwargs
     )
