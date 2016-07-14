73b6198b8d2aa888477f61d56a24931f875c9888
Christopher Bowman <cbowman0@gmail.com>
2016-07-14 13:08:08 -0400

    Add more coverage for render functions
    * Add tests for averageOutsidePercentile()
    * Add tests for removeBetweenPercentile()
    * Add tests for sortByName()
    * Add tests for sortByMaxima()
    * Add tests for sortByMinima()
    
    * Change name in transformNull when a referenceSeries is passed in.
    
    * Add tests for transform_null() with Reference Series passwed in
    * Add tests for isNonNull()
    * Add tests for identity()
    * Add tests for aggregateLine()
    * Add tests for threshold()

--- webapp/graphite/render/functions.py
+++ webapp/graphite/render/functions.py
@@ -2800,7 +2800,7 @@ def transformNull(requestContext, seriesList, default=0, referenceSeries=None):
 
   for series in seriesList:
     if referenceSeries:
-      series.name = "transformNull(%s,%g,referenceSeries)" % (series.name, default)
+      series.name = "transformNull(%s,%g,%s)" % (series.name, default, referenceSeries)
     else:
       series.name = "transformNull(%s,%g)" % (series.name, default)
     series.pathExpression = series.name
