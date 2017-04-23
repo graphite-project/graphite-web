dd521b3adad3a544a5d5115ce513f6f8b46ce8c0
Denis Zhdanov <denis.zhdanov@gmail.com>
2017-04-23 18:13:54 +0200

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ examples = [ ('examples', glob('examples/example-*')) ]
 try:
     setup(
       name='graphite-web',
-      version='1.0.0',
+      version='1.0.1',
       url='http://graphiteapp.org/',
       author='Chris Davis',
       author_email='chrismd@gmail.com',
