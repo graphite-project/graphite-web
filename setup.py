8606bc7a9c3e13482a99b18e4f41f88b431f9672
Christopher Bowman <cbowman0@gmail.com>
2017-06-17 17:50:04 -0400

    Django 1.8
    Add Django 1.8 to the tests and other relevant sections.

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ try:
         ['templates/*', 'local_settings.py.example']},
       scripts=glob('bin/*'),
       data_files=webapp_content.items() + storage_dirs + conf_files + examples,
-      install_requires=['Django>=1.9,<1.11.99', 'django-tagging==0.4.3', 'pytz', 'pyparsing', 'cairocffi', 'urllib3', 'scandir'],
+      install_requires=['Django>=1.8,<1.11.99', 'django-tagging==0.4.3', 'pytz', 'pyparsing', 'cairocffi', 'urllib3', 'scandir'],
       classifiers=[
           'Intended Audience :: Developers',
           'Natural Language :: English',
