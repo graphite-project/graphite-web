5af3120c8c06dca30bdb3a526bad76f29ac797ce
Christopher Bowman <cbowman0@gmail.com>
2017-10-13 11:45:49 -0400

    Add test for mostDeviant

--- webapp/tests/test_functions.py
+++ webapp/tests/test_functions.py
@@ -2164,6 +2164,26 @@ class FunctionsTest(TestCase):
 
         self.assertEqual(results, [inputSeries[1]])
 
+    def test_mostDeviant(self):
+        inputSeries = self._gen_series_list_with_data(
+            key=['test.foo', 'test.bar', 'test.baz', 'test.blah'],
+            start=0,
+            end=600,
+            step=60,
+            data=[[10,10,10,3,10,5,6,10,7,10,10], [0,1,2,3,4,5,6,7,8,9], [1,1,1,1,1,1,1,1,1,1], [5,5,5,5,5,5,5,5,5,5]]
+        )
+
+        results = functions.mostDeviant(
+            self._build_requestContext(
+                startTime=datetime(1970, 1, 1, 0, 10, 0, 0, pytz.timezone(settings.TIME_ZONE)),
+                endTime=datetime(1970, 1, 1, 0, 20, 0, 0, pytz.timezone(settings.TIME_ZONE))
+            ),
+            inputSeries,
+            2
+        )
+
+        self.assertEqual(results, [inputSeries[1], inputSeries[0]])
+
     def test_check_empty_lists(self):
         seriesList = []
         config = [[1000, 100, 10, 0], []]
