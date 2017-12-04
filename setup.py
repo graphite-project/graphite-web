54ae51ca41774da698f1f830f97476f8cfd4c85e
Thomas Kluyver <thomas@kluyver.me.uk>
2017-12-04 11:47:50 +0000

    Update classifiers for Python 3 compatibility

--- setup.py
+++ setup.py
@@ -113,7 +113,8 @@ try:
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 2 :: Only',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
           ],
       **setup_kwargs
     )
