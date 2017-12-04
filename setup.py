1ee9c8393dbca816d90338215241e28321b2dfaa
Thomas Kluyver <thomas@kluyver.me.uk>
2017-12-04 19:02:12 +0000

    Add Python 3.4, 3.5 to classifiers

--- setup.py
+++ setup.py
@@ -116,6 +116,8 @@ try:
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           ],
       **setup_kwargs
