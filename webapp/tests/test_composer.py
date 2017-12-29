15b6750cf62e33238dd38c64ea393f9dd36eb45b
Christopher Bowman <cbowman0@gmail.com>
2017-12-29 04:51:36 -0500

    Test composer's send_mail

--- webapp/tests/test_composer.py
+++ webapp/tests/test_composer.py
@@ -1,34 +1,34 @@
-import copy
-import errno
-import json
 import mock
-import os
 
-from . import TEST_CONF_DIR
+from urllib3.response import HTTPResponse
+from graphite.util import BytesIO
 
-from django.conf import settings
+from .base import TestCase
 try:
     from django.urls import reverse
 except ImportError:  # Django < 1.10
     from django.core.urlresolvers import reverse
-from django.http import HttpResponse
-from .base import TestCase
-from django.test.utils import override_settings
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-except ImportError:
-    from django.contrib.auth.models import User
 
 
 class ComposerTest(TestCase):
-    def test_send_email(self):
-        url = reverse('send_email')
-        request = {"sender": "noreply@localhost",
-                   "recipients": "noreply@localhost",
-                   "subject": "Test email",
-                   "message": "Here is the test graph",
-                   "graph_params": '{"target":["sumSeries(a.b.c.d)"],"title":"Test","width":"500","from":"-55minutes","until":"now","height":"400"}'}
-        response = self.client.post(url, request)
-        self.assertEqual(response.content, '{"success": true}')
- 
+    @mock.patch('six.moves.http_client.HTTPConnection.request')
+    @mock.patch('six.moves.http_client.HTTPConnection.getresponse')
+    @mock.patch('graphite.composer.views.SMTP')
+    @mock.patch('django.conf.settings.SMTP_SERVER', 'localhost')
+    def test_send_email(self, mock_smtp, http_response, http_request):
+        url = reverse('composer_send_email')
+        request = { "to": "noreply@localhost",
+                   "url": 'https://localhost:8000/render?target=sumSeries(a.b.c.d)&title=Test&width=500&from=-55minutes&until=now&height=400'}
+
+        response = self.client.get(reverse('render'), {'target': 'test'})
+        self.assertEqual(response['Content-Type'], 'image/png')
+        data = response.content
+        responseObject = HTTPResponse(body=BytesIO(data), status=200, preload_content=False)
+        http_request.return_value = responseObject
+        http_response.return_value = responseObject
+
+        instance = mock_smtp.return_value
+        instance.sendmail.return_value = {}
+
+        response = self.client.get(url, request)
+        self.assertEqual(response.content, 'OK')
