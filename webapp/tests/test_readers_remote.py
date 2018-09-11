ed795939b329f70d70ec438147c3ab9c55eb7bdf
Dan Cech <dcech@grafana.com>
2018-09-11 18:28:03 -0400

    move deserialize into remote finder

--- webapp/tests/test_readers_remote.py
+++ webapp/tests/test_readers_remote.py
@@ -148,7 +148,7 @@ class RemoteReaderTests(TestCase):
         responseObject = HTTPResponse(body=BytesIO(b'error'), status=200, preload_content=False)
         http_request.return_value = responseObject
 
-        with self.assertRaisesRegexp(Exception, 'Error decoding render response from http://[^ ]+: .+'):
+        with self.assertRaisesRegexp(Exception, 'Error decoding response from http://[^ ]+: .+'):
           reader.fetch(startTime, endTime)
 
         # invalid response data
