83d5ef6d93081ddc6785b225cbc162e3dda05610
deniszh <denis.zhdanov@gmail.com>
2018-11-04 09:54:44 +0100

    Proper fix

--- webapp/graphite/readers/rrd.py
+++ webapp/graphite/readers/rrd.py
@@ -27,7 +27,7 @@ class RRDReader(BaseReader):
     def _convert_fs_path(fs_path):
         if isinstance(fs_path, six.text_type):
             fs_path = fs_path.encode(sys.getfilesystemencoding())
-        return os.path.realpath(fs_path).decode("utf-8")
+        return os.path.realpath(fs_path).decode(sys.getfilesystemencoding())
 
     def __init__(self, fs_path, datasource_name):
         self.fs_path = RRDReader._convert_fs_path(fs_path)
