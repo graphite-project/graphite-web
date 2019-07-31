8af0548b09b73201bccab5681822cdb9443c687f
Mauro Stettler <mauro.stettler@gmail.com>
2019-07-31 14:26:28 -0400

    add functions to validate params based on what functions declare they expect

--- webapp/graphite/functions/params.py
+++ webapp/graphite/functions/params.py
@@ -1,3 +1,8 @@
+import six
+
+from graphite.render.attime import parseTimeOffset
+
+
 class ParamTypes(object):
   pass
 
@@ -19,6 +24,41 @@ for paramType in [
   'tag',
 ]:
   setattr(ParamTypes, paramType, paramType)
+
+
+def validateBoolean(value):
+  return isinstance(value, bool)
+
+
+def validateFloat(value):
+  return isinstance(value, float)
+
+
+def validateInteger(value):
+  return isinstance(value, six.integer_types)
+
+
+def validateInterval(value):
+  try:
+    parseTimeOffset(value)
+  except Exception:
+    return False
+  return True
+
+
+def validateSeriesList(value):
+  return isinstance(value, list)
+
+
+typeValidators = {
+  'boolean': validateBoolean,
+  'float': validateFloat,
+  'integer': validateInteger,
+  'interval': validateInterval,
+  'node': validateInteger,
+  'seriesList': validateSeriesList,
+  'seriesLists': validateSeriesList,
+}
 
 
 class Param(object):
@@ -52,3 +92,45 @@ class Param(object):
     if self.suggestions:
       jsonVal['suggestions'] = self.suggestions
     return jsonVal
+
+  def validateValue(self, value):
+    validator = typeValidators.get(self.type, None)
+    if validator is not None:
+      return validator(value)
+
+    # if there's no validator for the type we assume True
+    return True
+
+
+def satisfiesParams(params, args, kwargs):
+  valid_args = []
+
+  if len(params) == 0 or params[len(params)-1].multiple is False:
+    if len(args) + len(kwargs) > len(params):
+      return False
+
+  for i in range(len(params)):
+    if len(args) <= i:
+      # requirement is satisfied from "kwargs"
+      value = kwargs.get(params[i].name, None)
+      if value is None:
+        if params[i].required:
+          # required parameter is missing
+          return False
+        else:
+          # got multiple values for keyword argument
+          if params[i].name in valid_args:
+            return False
+    else:
+      # requirement is satisfied from "args"
+      value = args[i]
+
+    # parameter is restricted to a defined set of values, but value is not in it
+    if params[i].options and value not in params[i].options:
+      return False
+
+    params[i].validateValue(value)
+
+    valid_args.append(params[i].name)
+
+  return True
