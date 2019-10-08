5a8c97ce7f3cec39a779864fe6ccd52cb2a1ec68
Dieter Plaetinck <dieter@grafana.com>
2019-10-08 10:02:42 -0400

    fix functions that aggregate to include the aliases in their params
    This should fix the validation not being aware of legal aliases.
    fix #2494

--- webapp/graphite/render/functions.py
+++ webapp/graphite/render/functions.py
@@ -286,7 +286,7 @@ aggFuncAliases = {
   'current': aggFuncs['last'],
 }
 
-aggFuncNames = sorted(aggFuncs.keys())
+aggFuncNames = sorted(aggFuncs.keys() + aggFuncAliases.keys())
 
 
 def getAggFunc(func, rawFunc=None):
