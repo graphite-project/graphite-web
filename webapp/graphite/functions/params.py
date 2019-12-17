52c0fd11a18de633e28e389d394ceba45a8e2831
Mauro Stettler <mauro.stettler@gmail.com>
2019-12-17 12:37:30 -0300

    relax enforcement of options sets in validation (#2513)
    * relax enforcement of options sets in validation
    
    * move safe* and agg helper funcs into distinct file

--- webapp/graphite/functions/params.py
+++ webapp/graphite/functions/params.py
@@ -2,29 +2,30 @@ import six
 
 from graphite.errors import InputParameterError
 from graphite.render.attime import parseTimeOffset
+from graphite.logger import log
+from graphite.functions.aggfuncs import aggFuncs, aggFuncAliases
 
 
 class ParamTypes(object):
   pass
 
 
-for paramType in [
-  'aggFunc',
-  'boolean',
-  'date',
-  'float',
-  'integer',
-  'interval',
-  'intOrInterval',
-  'node',
-  'nodeOrTag',
-  'series',
-  'seriesList',
-  'seriesLists',
-  'string',
-  'tag',
-]:
-  setattr(ParamTypes, paramType, paramType)
+class ParamType(object):
+
+  def __init__(self, name, validator=None):
+    self.name = name
+    self.validator = validator
+
+  @classmethod
+  def register(cls, name, *args):
+    setattr(ParamTypes, name, cls(name, *args))
+
+  def isValid(self, value):
+    if self.validator is None:
+      # if there's no validator for the type we assume True
+      return True
+
+    return self.validator(value)
 
 
 def validateBoolean(value):
@@ -51,15 +52,50 @@ def validateSeriesList(value):
   return isinstance(value, list)
 
 
-typeValidators = {
-  'boolean': validateBoolean,
-  'float': validateFloat,
-  'integer': validateInteger,
-  'interval': validateInterval,
-  'node': validateInteger,
-  'seriesList': validateSeriesList,
-  'seriesLists': validateSeriesList,
-}
+ParamType.register('boolean', validateBoolean)
+ParamType.register('date')
+ParamType.register('float', validateFloat)
+ParamType.register('integer', validateInteger)
+ParamType.register('interval', validateInterval)
+ParamType.register('intOrInterval')
+ParamType.register('node', validateInteger)
+ParamType.register('nodeOrTag')
+ParamType.register('series')
+ParamType.register('seriesList', validateSeriesList)
+ParamType.register('seriesLists', validateSeriesList)
+ParamType.register('string')
+ParamType.register('tag')
+
+
+class ParamTypeAggFunc(ParamType):
+
+  def __init__(self, name):
+    super(ParamTypeAggFunc, self).__init__(name=name, validator=self.validateAggFuncs)
+
+  @classmethod
+  def getValidAggFuncs(cls):
+    return list(aggFuncs.keys()) + list(aggFuncAliases.keys())
+
+  @classmethod
+  def getDeprecatedAggFuncs(cls):
+    return [name + 'Series' for name in cls.getValidAggFuncs()]
+
+  @classmethod
+  def getAllValidAggFuncs(cls):
+    return cls.getValidAggFuncs() + cls.getDeprecatedAggFuncs()
+
+  def validateAggFuncs(self, value):
+    if value in self.getValidAggFuncs():
+      return True
+
+    if value in self.getDeprecatedAggFuncs():
+      log.warning('Deprecated aggregation function "{value}" used'.format(value=value))
+      return True
+
+    return False
+
+
+ParamTypeAggFunc.register('aggFunc')
 
 
 class Param(object):
@@ -68,7 +104,7 @@ class Param(object):
   def __init__(self, name, paramtype, required=False, default=None, multiple=False, options=None,
                suggestions=None):
     self.name = name
-    if not hasattr(ParamTypes, paramtype):
+    if not isinstance(paramtype, ParamType):
       raise Exception('Invalid type %s for parameter %s' % (paramtype, name))
     self.type = paramtype
     self.required = bool(required)
@@ -80,7 +116,7 @@ class Param(object):
   def toJSON(self):
     jsonVal = {
       'name': self.name,
-      'type': self.type,
+      'type': self.type.name,
     }
     if self.required:
       jsonVal['required'] = True
@@ -95,16 +131,14 @@ class Param(object):
     return jsonVal
 
   def validateValue(self, value):
+    if value is None and self.default is not None:
+      value = self.default
+
     # None is ok for optional params
     if not self.required and value is None:
       return True
 
-    validator = typeValidators.get(self.type, None)
-    # if there's no validator for the type we assume True
-    if validator is None:
-      return True
-
-    return validator(value)
+    return self.type.isValid(value)
 
 
 def validateParams(func, params, args, kwargs):
@@ -138,13 +172,13 @@ def validateParams(func, params, args, kwargs):
     # parameter is restricted to a defined set of values, but value is not in it
     if params[i].options and value not in params[i].options:
       raise InputParameterError(
-        'Invalid option specified for function "{func}" parameter "{param}"'.format(
-          param=params[i].name, func=func))
+        'Invalid option "{value}" specified for function "{func}" parameter "{param}"'.format(
+          value=value, param=params[i].name, func=func))
 
     if not params[i].validateValue(value):
       raise InputParameterError(
-        'Invalid {type} value specified for function "{func}" parameter "{param}"'.format(
-          type=params[i].type, param=params[i].name, func=func))
+        'Invalid {type} value specified for function "{func}" parameter "{param}": {value}'.format(
+          type=params[i].type.name, func=func, param=params[i].name, value=value))
 
     valid_args.append(params[i].name)
 
