da290e8c4a8502c10bbc089ac92bfc4d4b89b523
replay <mauro.stettler@gmail.com>
2020-05-06 14:01:06 -0300

    raise exception when not recoverable

--- webapp/graphite/render/evaluator.py
+++ webapp/graphite/render/evaluator.py
@@ -130,6 +130,7 @@ def evaluateTokens(requestContext, tokens, replacements=None, pipedArg=None):
       return []
     except InputParameterError as e:
       handleInvalidParameters(e)
+      raise
 
   return evaluateScalarTokens(tokens)
 
