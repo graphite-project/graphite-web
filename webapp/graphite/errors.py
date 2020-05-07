23b590627244c5b26801bd23e0d68c0521e04559
replay <mauro.stettler@gmail.com>
2020-05-07 14:53:05 -0300

    use different handlers for exceptions
    (cherry picked from commit 798de47b7831a73f1922213a59805a7c17c4f249)

--- webapp/graphite/errors.py
+++ webapp/graphite/errors.py
@@ -94,11 +94,6 @@ class InputParameterError(ValueError):
         return msg
 
 
-# subtype of input parameter error which only gets raised by the parameter validation
-class InputValidationError(InputParameterError):
-    pass
-
-
 # decorator which turns InputParameterExceptions into Django's HttpResponseBadRequest
 def handleInputParameterError(f):
     def new_f(*args, **kwargs):
