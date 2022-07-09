924de472a5b1b2fb8d6c2a709f7e5bc9165e9a8a
Denys Zhdanov <denys.zhdanov@booking.com>
2022-07-09 17:37:14 +0200

    Fix admin.W411

--- webapp/graphite/app_settings.py
+++ webapp/graphite/app_settings.py
@@ -53,6 +53,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
+                'django.template.context_processors.request',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
