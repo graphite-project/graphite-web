6a469c9f10f9333bebfe85b4a1178e3826dc83fe
akj0811-rubrik <AnkitKumar.Jain@rubrik.com>
2024-06-17 07:46:17 +0000

    Fix webapp/graphite/urls.py

--- webapp/graphite/urls.py
+++ webapp/graphite/urls.py
@@ -12,32 +12,33 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License"""
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import re_path
 from django.contrib import admin
 from graphite.url_shortener.views import shorten, follow
 from graphite.browser.views import browser
 
 graphite_urls = [
-    url('^admin/', admin.site.urls),
-    url('^render', include('graphite.render.urls')),
-    url('^composer', include('graphite.composer.urls')),
-    url('^metrics', include('graphite.metrics.urls')),
-    url('^browser', include('graphite.browser.urls')),
-    url('^account', include('graphite.account.urls')),
-    url('^dashboard', include('graphite.dashboard.urls')),
-    url('^whitelist', include('graphite.whitelist.urls')),
-    url('^version', include('graphite.version.urls')),
-    url('^events', include('graphite.events.urls')),
-    url('^tags', include('graphite.tags.urls')),
-    url('^functions', include('graphite.functions.urls')),
-    url('^s/(?P<path>.*)', shorten, name='shorten'),
-    url('^S/(?P<link_id>[a-zA-Z0-9]+)/?$', follow, name='follow'),
-    url('^$', browser, name='browser'),
+    re_path('^admin/', admin.site.urls),
+    re_path('^render', include('graphite.render.urls')),
+    re_path('^composer', include('graphite.composer.urls')),
+    re_path('^metrics', include('graphite.metrics.urls')),
+    re_path('^browser', include('graphite.browser.urls')),
+    re_path('^account', include('graphite.account.urls')),
+    re_path('^dashboard', include('graphite.dashboard.urls')),
+    re_path('^whitelist', include('graphite.whitelist.urls')),
+    re_path('^version', include('graphite.version.urls')),
+    re_path('^events', include('graphite.events.urls')),
+    re_path('^tags', include('graphite.tags.urls')),
+    re_path('^functions', include('graphite.functions.urls')),
+    re_path('^s/(?P<path>.*)', shorten, name='shorten'),
+    re_path('^S/(?P<link_id>[a-zA-Z0-9]+)/?$', follow, name='follow'),
+    re_path('^$', browser, name='browser'),
 ]
 
 if settings.URL_PREFIX.strip('/'):
     urlpatterns = [
-        url(r'^{0}/'.format(settings.URL_PREFIX.strip('/')), include(graphite_urls)),
+        re_path(r'^{0}/'.format(settings.URL_PREFIX.strip('/')), include(graphite_urls)),
     ]
 else:
     urlpatterns = graphite_urls
